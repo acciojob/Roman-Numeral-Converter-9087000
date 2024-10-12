069cd255faa2af950647cac5fe865d48d474ee40
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 18:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
 function convertToRoman(num) {
-  	const obj = {
-       0:['M',1000], 
-        1:['CM',900], // 900
-        2:['D', 500], 
-        3:['CD',400], // 400
-        4:['C', 100], 
-        5:['XC',90],  // 90
-        6:['L', 50], 
-        7:['XL',40],  // 40
-        8:['X', 10], 
-        9:['IX',9],   // 9
-        10:['V', 5], 
-        11:['IV',4],  // 4
-        12:['I', 1]
-    };
+    // Array of Roman numerals and their corresponding values
+    const romanSymbols = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
-  //your code here
-let result='';
+    // Result string for the Roman numeral
+    let result = '';
 
-       
-        for(let i=0;i<Object.keys(obj).length;i++){
-           
-            while(num>=obj[i][1]){
-                num=num-obj[i][1];
-                result=result+obj[i][0];
-               
-            }
-            
+    // Loop through each symbol
+    for (let i = 0; i < romanSymbols.length; i++) {
+        const [symbol, value] = romanSymbols[i];
+        
+        // Append the symbol while the value can be subtracted from num
+        while (num >= value) {
+            result += symbol;
+            num -= value;
         }
-        
-        return result;
-        
+    }
+
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
-
-
+// Example test cases
+console.log(convertToRoman(14)); // XIV
+console.log(convertToRoman(798)); // DCCXCVIII
 
 
 // do not edit below this line
 module.exports = convertToRoman
-
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
